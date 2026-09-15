bac49bb53486c3162d851763cec58ce9e3489dc8
konieczniak08@gmail.com <konieczniak08@gmail.com>
2026-09-15 03:45:55 +0000

    Make null pointer pattern easier to complete

--- src/patterns.js
+++ src/patterns.js
@@ -1,10 +1,10 @@
 // Coding error patterns (5x5)
 const ERROR_PATTERNS = [
-  {{
+  {
   name: "Null Pointer",
   pattern: [
     [0, 1, 1, 1, 0],
-    [0, 1, 0, 0, 0],
+    [0, 1, 0, 1, 0],
     [0, 1, 1, 1, 0],
     [0, 0, 1, 0, 0],
     [0, 0, 1, 0, 0],
